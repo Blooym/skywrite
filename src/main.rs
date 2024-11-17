e9ecc6601ad45afd6d03c056e1f08183b78534d0
Blooym <19539165+Blooym@users.noreply.github.com>
2024-11-17 07:09:44 +0000

    chore: stop logging thread names

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,6 @@ async fn main() -> Result<()> {
     dotenv().ok();
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::try_from_default_env().unwrap_or(EnvFilter::new("info")))
-        .with_thread_names(true)
         .with_thread_ids(true)
         .init();
 
